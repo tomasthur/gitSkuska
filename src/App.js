f44a42bdb3422a2d432ab45339c721cb82cfdaff
Tomas Thur <thur.tomas@gmail.com>
2022-06-10 10:09:55 +0200

    add some buttons from MUI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import "./App.scss";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
@@ -8,6 +15,22 @@ function App() {
         <button>Skuska tlacidla</button>
         <button>dalsia skuska</button>
       </div>
+      <Button
+        style={{
+          backgroundColor: "#ffea00",
+          fontSize: "12px",
+          margin: "4px",
+          alignItems: "center",
+        }}
+      >
+        Secondary
+      </Button>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
     </div>
   );
 }
